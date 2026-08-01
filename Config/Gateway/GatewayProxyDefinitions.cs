ac817ab54f67b5ed293ea729f6e53c267397baa2
JJTransportes <comercialpepiviagens@gmail.com>
2026-08-01 16:12:27 -0300

    feat: add services application to proxy configuration.

--- Config/Gateway/GatewayProxyDefinitions.cs
+++ Config/Gateway/GatewayProxyDefinitions.cs
@@ -1,30 +1,41 @@
 using Gateway.Config;
 using Gateway.Config.Api;
 using Gateway.Config.Auth;
+using Gateway.Config.Services;
 using Yarp.ReverseProxy.Configuration;
 
 class GatewayProxyDefinitions(
-IProxyDefinitions<ApiProxyDefinitions> apiProxyDefinitions,
-IProxyDefinitions<AuthProxyDefinitions> authProxyDefinitions
+IProxyDefinitions<ServicesProxyDefinitions> servicesProxyDefinitions,
+IProxyDefinitions<AuthProxyDefinitions> authProxyDefinitions,
+IProxyDefinitions<ApiProxyDefinitions> apiProxyDefinitions
 ) : IProxyDefinitions<GatewayProxyDefinitions>
 {
   public List<ClusterConfig> GetClusters()
   {
-    List<ClusterConfig> apiClusters = apiProxyDefinitions.GetClusters();
+    List<ClusterConfig> servicesClusters = servicesProxyDefinitions.GetClusters();
     List<ClusterConfig> authClusters = authProxyDefinitions.GetClusters();
+    List<ClusterConfig> apiClusters = apiProxyDefinitions.GetClusters();
 
-    List<ClusterConfig> Clusters = [.. authClusters, .. apiClusters];
+    List<ClusterConfig> Clusters = [
+      .. servicesClusters,
+      .. authClusters,
+      .. apiClusters
+      ];
 
     return Clusters;
   }
 
   public List<RouteConfig> GetRoutes()
   {
-
+    List<RouteConfig> servicesRoutes = servicesProxyDefinitions.GetRoutes();
     List<RouteConfig> apiRoutes = apiProxyDefinitions.GetRoutes();
     List<RouteConfig> authRoutes = authProxyDefinitions.GetRoutes();
 
-    List<RouteConfig> Routes = [.. authRoutes, .. apiRoutes];
+    List<RouteConfig> Routes = [
+      .. servicesRoutes,
+      .. authRoutes,
+      .. apiRoutes
+      ];
 
     return Routes;
   }
--- Program.cs
+++ Program.cs
@@ -1,26 +1,32 @@
 using Gateway.Config;
 using Gateway.Config.Api;
 using Gateway.Config.Auth;
+using Gateway.Config.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+IConfigurationSection servicesSection = builder.Configuration.GetSection(ServicesConfig.SectionName);
 IConfigurationSection authSection = builder.Configuration.GetSection(AuthConfig.SectionName);
 IConfigurationSection apiSection = builder.Configuration.GetSection(ApiConfig.SectionName);
 IConfigurationSection appSection = builder.Configuration.GetSection(AppConfig.SectionName);
+builder.Services.Configure<ServicesConfig>(servicesSection);
 builder.Services.Configure<AuthConfig>(authSection);
 builder.Services.Configure<ApiConfig>(apiSection);
 builder.Services.Configure<AppConfig>(appSection);
 
+var servicesConfig = authSection.Get<ServicesConfig>();
 var authConfig = authSection.Get<AuthConfig>();
 var apiConfig = apiSection.Get<ApiConfig>();
 var appConfig = appSection.Get<AppConfig>();
 
+if (servicesConfig is null) throw new Exception("ServicesConfig not provided.");
 if (authConfig is null) throw new Exception("AuthConfig not provided.");
 if (apiConfig is null) throw new Exception("ApiConfig not provided.");
 if (appConfig is null) throw new Exception("AppConfig not provided.");
 
 builder.WebHost.UseUrls($"http://*:{appConfig.Port}");
 
+builder.Services.AddSingleton<IProxyDefinitions<ServicesProxyDefinitions>, ServicesProxyDefinitions>();
 builder.Services.AddSingleton<IProxyDefinitions<AuthProxyDefinitions>, AuthProxyDefinitions>();
 builder.Services.AddSingleton<IProxyDefinitions<ApiProxyDefinitions>, ApiProxyDefinitions>();
 builder.Services.AddSingleton<IProxyDefinitions<GatewayProxyDefinitions>, GatewayProxyDefinitions>();
