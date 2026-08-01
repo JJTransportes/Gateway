8a03ebc60a5dd89879ccafdf6d652ba37853d894
JJTransportes <comercialpepiviagens@gmail.com>
2026-08-01 16:00:12 -0300

    feat: configure new auth section and add dependency injection to auth, api and gateway proxy definitions.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,29 @@
 using Gateway.Config;
+using Gateway.Config.Api;
+using Gateway.Config.Auth;
 
 var builder = WebApplication.CreateBuilder(args);
 
+IConfigurationSection authSection = builder.Configuration.GetSection(AuthConfig.SectionName);
 IConfigurationSection apiSection = builder.Configuration.GetSection(ApiConfig.SectionName);
 IConfigurationSection appSection = builder.Configuration.GetSection(AppConfig.SectionName);
+builder.Services.Configure<AuthConfig>(authSection);
 builder.Services.Configure<ApiConfig>(apiSection);
 builder.Services.Configure<AppConfig>(appSection);
 
-var apiConfig = appSection.Get<ApiConfig>();
+var authConfig = authSection.Get<AuthConfig>();
+var apiConfig = apiSection.Get<ApiConfig>();
 var appConfig = appSection.Get<AppConfig>();
 
+if (authConfig is null) throw new Exception("AuthConfig not provided.");
 if (apiConfig is null) throw new Exception("ApiConfig not provided.");
 if (appConfig is null) throw new Exception("AppConfig not provided.");
 
 builder.WebHost.UseUrls($"http://*:{appConfig.Port}");
+
+builder.Services.AddSingleton<IProxyDefinitions<AuthProxyDefinitions>, AuthProxyDefinitions>();
+builder.Services.AddSingleton<IProxyDefinitions<ApiProxyDefinitions>, ApiProxyDefinitions>();
+builder.Services.AddSingleton<IProxyDefinitions<GatewayProxyDefinitions>, GatewayProxyDefinitions>();
 
 builder.Services.AddOpenApi();
 builder.Services.UseReverseProxy();
